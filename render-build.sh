5249e43af994ab7bc51c1cb5644d85c4ce029420
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2025-12-16 09:44:40 -0600

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install ffmpeg (Render's base image supports apt-get)
-apt-get update
-apt-get install -y ffmpeg
-
-# Upgrade pip
-python -m pip install --upgrade pip
-
-# Install Python deps
+pip install --upgrade pip
 pip install -r requirements.txt
